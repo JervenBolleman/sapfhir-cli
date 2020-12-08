f522b16244fb7b79a34443d3c956e80f2d38c934
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-12-08 21:30:23 +0100

    Simple CLI tool to test sapfhir

---
+++ README.md
@@ -0,0 +1,46 @@
+# SapFhir-CLI
+
+Join genomic variation graphs with public data or internal medical data e.g. FHIR.
+by having a FAIR data access, using W3C sparql as a standard protocol.
+
+This is a command line tool to quickly test the concept.
+
+
+# Building
+
+You need local checkouts of 
+
+[handlegraph4j](https://github.com/JervenBolleman/handlegraph4j)
+[handlegraph4jGFA](https://github.com/JervenBolleman/handlegraph4jGFA)
+[handlegraph4jRDF](https://github.com/JervenBolleman/handlegraph4jRDF)
+[handlegraph4j-simple](https://github.com/JervenBolleman/handlegraph4j-simple)
+[sapfhir](https://github.com/JervenBolleman/sapfhir)
+
+Build them with maven on java 11+
+
+```
+for i in handlegraph4j handlegraph4jGFA handlegraph4jRDF handlegraph4j-simple sapfhir
+cd $i
+    mvn install
+cd ..
+```
+
+```
+mvn assembly:assembly
+```
+
+# Run
+
+```
+java -jar target/sapfhir-cli-1.0-SNAPSHOT-jar-with-dependencies.jar \
+    --gfa ~/git/odgi/test/t.gfa \
+    -Xmx12g \
+    "PREFIX vg:<http://biohackathon.org/resource/vg#> SELECT ?path WHERE {?path a vg:Path}"
+```
+
+# Status
+
+This is a [RDF4j](https://rdf4j.org/) SAIL implementation that can take any handlegraph4j 
+implementation and represent it as a [W3C sparql 1.1](https://www.w3.org/TR/sparql11-query/) endpoint. 
+
+It is beleived to be functionally complete.
---
+++ src/main/java/swiss/sib/swissprot/sapfhir/cli/CLI.java
@@ -0,0 +1,102 @@
+package swiss.sib.swissprot.sapfhir.cli;
+
+import io.github.vgteam.handlegraph4j.gfa1.GFA1Reader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.concurrent.Callable;
+import java.util.stream.Stream;
+
+import org.eclipse.rdf4j.query.BooleanQuery;
+import org.eclipse.rdf4j.query.GraphQuery;
+import org.eclipse.rdf4j.query.MalformedQueryException;
+import org.eclipse.rdf4j.query.Query;
+import org.eclipse.rdf4j.query.QueryEvaluationException;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResultHandlerException;
+import org.eclipse.rdf4j.query.resultio.BooleanQueryResultFormat;
+import org.eclipse.rdf4j.query.resultio.QueryResultIO;
+import org.eclipse.rdf4j.query.resultio.QueryResultWriter;
+import org.eclipse.rdf4j.query.resultio.text.csv.SPARQLResultsCSVWriter;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.rio.RDFHandler;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
+import org.eclipse.rdf4j.rio.turtle.TurtleWriter;
+import org.eclipse.rdf4j.sail.SailException;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import swiss.sib.swissprot.handlegraph4j.simple.SimplePathGraph;
+import swiss.sib.swissprot.handlegraph4j.simple.builders.SimplePathGraphFromGFA1Builder;
+import swiss.sib.swissprot.sapfhir.sparql.PathHandleGraphSail;
+
+/**
+ * A basic CLI that takes a SPARQL query and tries to translate owl:sameAs IRI
+ * patterns using current data from the Identifier.org API.
+ *
+ * @author Jerven Bolleman <jerven.bolleman@sib.swiss>
+ */
+@Command(name = "sparqlGfa",
+        mixinStandardHelpOptions = true,
+        version = "Run a sparql query against a GFA",
+        description = "Sparql queryies against a GFA")
+
+public class CLI implements Callable<Integer> {
+
+    @Option(names = {"-g", "--gfa"})
+    public File gfaFile;
+
+    @Option(names = {"-b", "--base"}, defaultValue = "http://example.org/vg/")
+    public String base;
+
+    @Parameters(index = "0", description = "The SPARQL query to test")
+    public String query;
+
+    @Override
+    public Integer call() throws IOException {
+        SimplePathGraph spg = loadGFA(gfaFile);
+        var rep = new PathHandleGraphSail<>(spg, base);
+        try {
+            SailRepository sr = new SailRepository(rep);
+            rep.initialize();
+            Query pTQ = sr.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, query);
+            if (pTQ instanceof TupleQuery) {
+                SPARQLResultsCSVWriter handler = new SPARQLResultsCSVWriter(System.out);
+                ((TupleQuery) pTQ).evaluate(handler);
+            } else if (pTQ instanceof GraphQuery) {
+                RDFHandler createWriter = new TurtleWriter(System.out);
+                ((GraphQuery) pTQ).evaluate(createWriter);
+            } else if (pTQ instanceof BooleanQuery) {
+                QueryResultWriter createWriter = QueryResultIO.createWriter(BooleanQueryResultFormat.TEXT, System.out);
+                boolean evaluate = ((BooleanQuery) pTQ).evaluate();
+                createWriter.handleBoolean(evaluate);
+            }
+            return 0;
+        } catch (MalformedQueryException e) {
+            System.err.println("Query syntax is broken");
+            return 2;
+        } catch (SailException | QueryEvaluationException | RDFHandlerException | TupleQueryResultHandlerException
+                | RepositoryException e) {
+            System.err.println("failed in rdf4j/sapfhir code");
+            return 1;
+        }
+    }
+
+    private SimplePathGraph loadGFA(File gfaFile) throws IOException {
+        try ( Stream<String> lines = Files.lines(gfaFile.toPath(), StandardCharsets.US_ASCII)) {
+            SimplePathGraphFromGFA1Builder simplePathGraphFromGFA1Builder = new SimplePathGraphFromGFA1Builder();
+            simplePathGraphFromGFA1Builder.parse(new GFA1Reader(lines.iterator()));
+            return simplePathGraphFromGFA1Builder.build();
+        }
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new CLI()).execute(args);
+        System.exit(exitCode);
+    }
+
+}
